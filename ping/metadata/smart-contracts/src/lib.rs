84d85685878b73dbecca9b72b3db728fa4ee9794
Luis Alberto Ramirez Albino <lara@192.168.1.5>
2024-08-03 22:43:21 -0600

    Basic structure of a smart contract

---
+++ ping/metadata/smart-contracts/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+use gstd::{debug, msg, prelude::*};
+
+#[no_mangle]
+extern "C" fn handle() {
+    msg::reply(String::from("Add Your Custom Message"), 0)
+        .expect("Error in sending a reply message");
+}
+
+#[no_mangle]
+extern "C" fn init() {
+    let init_message: String = msg::load().expect("Can't load init message");
+    debug!("Program was initialized with message: {:?}", init_message);
+}
